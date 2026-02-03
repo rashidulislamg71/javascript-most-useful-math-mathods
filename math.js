cb0aa450f491dd7a4d1a597430104c04795dd9d1
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-02-03 12:21:02 +0600

    js math methods

---
+++ math.js
@@ -0,0 +1,52 @@
+
+
+// Math.random()
+const randomNumber = Math.random()
+console.log(randomNumber);
+// 6 code otp generate
+const otp = Math.floor(100000 + Math.random() * 900000);
+console.log(otp);
+
+// Math.max()
+const maxNum = Math.max(12,32,53,64);
+console.log(maxNum);
+
+// Math.min()
+const minNum = Math.min(12,32,53,64)
+console.log(minNum);
+
+// Math.round()
+const roundNum = Math.round( 23.3);
+const roundNum2 = Math.round( 23.5);
+console.log(roundNum);
+console.log(roundNum2);
+
+// Math.abs()
+const absNum = Math.abs(-23);
+const absNum2 = Math.abs(33.23);
+console.log(absNum);
+console.log(absNum2);
+
+// Math.ceil()
+const ceilNum = Math.ceil(34.1);
+console.log(ceilNum) // output : 35
+
+// Math.floor()
+const floorNum = Math.floor(20.5);
+console.log(floorNum); // output: 20
+
+// Math.trunc()
+const truncNum = Math.trunc(50.534);
+console.log(truncNum); // output: 50
+
+// Math.sqrt()
+const sqrtNum = Math.sqrt(4);
+const sqrtNum2 = Math.sqrt(50);
+console.log(sqrtNum);
+console.log(sqrtNum2);
+
+// Math.pow()
+const powNum = Math.pow(4, 2);
+console.log(powNum);
+
+
